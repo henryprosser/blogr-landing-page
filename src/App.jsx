e46f0c19e3d0a8f9b09a0001eaab5630596b7c8b
henryprosser <henryprosser@yahoo.co.uk>
2025-08-05 16:10:09 -0700

    Add Tools section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import Future from "./components/Future";
 import Infrastructure from "./components/Infrastructure";
+import Tools from "./components/Tools";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
         <Hero />
         <Future />
         <Infrastructure />
+        <Tools />
       </main>
     </>
   );
