7505510371fcf2678f71d5184500b9238cf0c78b
henryprosser <henryprosser@yahoo.co.uk>
2025-08-29 14:46:37 -0700

    Implement responsive Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,65 +1,51 @@
 export default function Footer() {
+  const menuItems = [
+    {
+      title: "Product",
+      subMenu: [
+        "Overview",
+        "Pricing",
+        "Marketplace",
+        "Features",
+        "Integrations",
+      ],
+    },
+    {
+      title: "Company",
+      subMenu: ["About", "Team", "Blog", "Careers"],
+    },
+    {
+      title: "Connect",
+      subMenu: ["Contact", "Newsletter", "LinkedIn"],
+    },
+  ];
+
   return (
-    <footer className="grid grid-cols-1 justify-items-center bg-gray-950 rounded-tr-[6.25rem] px-[24px] py-[72px] gap-[72px]">
+    <footer className="grid grid-cols-1 md:grid-cols-[1fr_3fr] justify-items-center md:justify-items-start bg-gray-950 rounded-tr-[6.25rem] px-[24px] md:pl-[40px] md:pr-[80px] lg:px-[163px] py-[72px] gap-[72px]">
       <img
         src="/assets/logo.svg"
         className="h-[40px] w-auto"
         alt="Blogr logo"
       />
-      <div className="flex flex-col gap-[48px]">
-        <div className="flex flex-col gap-[32px] text-center">
-          <h4 className="text-preset-6 text-white">Product</h4>
-          <ul className="text-preset-7 text-white opacity-75">
-            <li>
-              <a href="#">Overview</a>
-            </li>
-            <li>
-              <a href="#">Pricing</a>
-            </li>
-            <li>
-              <a href="#">Marketplace</a>
-            </li>
-            <li>
-              <a href="#">Features</a>
-            </li>
-            <li>
-              <a href="#">Integrations</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="flex flex-col gap-[32px] text-center">
-          <h4 className="text-preset-6 text-white">Company</h4>
-          <ul className="text-preset-7 text-white opacity-75">
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Team</a>
-            </li>
-            <li>
-              <a href="#">Blog</a>
-            </li>
-            <li>
-              <a href="#">Careers</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="flex flex-col gap-[32px] text-center">
-          <h4 className="text-preset-6 text-white">Connect</h4>
-          <ul className="text-preset-7 text-white opacity-75">
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li>
-              <a href="#">Newsletter</a>
-            </li>
-            <li>
-              <a href="#">LinkedIn</a>
-            </li>
-          </ul>
-        </div>
+      <div
+        className="flex flex-col md:flex-row"
+        style={{ gap: "clamp(3rem, -0.8952rem + 16.62vw, 14.0625rem)" }}
+      >
+        {menuItems.map(({ title, subMenu }) => (
+          <div
+            key={title}
+            className="flex flex-col gap-[32px] text-center md:text-left"
+          >
+            <h4 className="text-preset-6 text-white">{title}</h4>
+            <ul className="text-preset-7 text-white opacity-75">
+              {subMenu.map((item) => (
+                <li key={item}>
+                  <a href="#">{item}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </footer>
   );
